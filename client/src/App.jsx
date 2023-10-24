9282a5f9662ae60dee7c9623f7fdd5cc6149853a
The-GO0d-Guy <malaha814@gmail.com>
2023-10-24 23:33:07 +0530

    component-structure-update-and-routes-update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,23 @@
 import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
 function App() {
 
   return (
-    <>
-      <div className="text-3xl">
-        Hello World !
-       </div>
-    </>
-  )
+    <div className="no-scroll">
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+    </Router>
+    </div>
+  );
 }
 
 export default App
