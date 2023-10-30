2dc50ccc936a772162119c01a88788c1adf4df01
The-GO0d-Guy <malaha814@gmail.com>
2023-10-30 23:56:44 +0530

    set-up-signUp-Funtionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const res = await fetch('/api/auth/signup',
+    {
+      method:'POST',
+      headers: {
+        'Content-Type': 'applications/json',
+      },
+      body:JSON.stringify(formData),
+    }
+    )
+  };
+
+  console.log(formData);
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Sign Up</h1>
@@ -11,20 +35,26 @@ export default function SignUp() {
           placeholder="Username"
           className="border p-3 rounded-lg"
           id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
           className="border p-3 rounded-lg"
           id="email"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           className="border p-3 rounded-lg"
           id="password"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80 text-lg">
+        <button
+          className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80 text-lg"
+          onClick={handleSubmit}
+        >
           SIGN UP
         </button>
       </form>
